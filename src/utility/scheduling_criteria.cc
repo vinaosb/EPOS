89de6cda5818a3620d0f34b1179e85ec64ce9832
vinicius <vinaosb@gmail.com>
2019-06-23 21:53:28 -0300

    c

--- src/utility/scheduling_criteria.cc
+++ src/utility/scheduling_criteria.cc
@@ -5,6 +5,9 @@
 
 __BEGIN_UTIL
 
+// Class attributes
+volatile unsigned int Scheduling_Criteria::Variable_Queue::_next_queue;
+
 // The following Scheduling Criteria depend on Alarm, which is not available at scheduler.h
 namespace Scheduling_Criteria
 {
